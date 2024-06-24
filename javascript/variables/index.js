d876fdf07a9d54236be6dfaeda93af5c2b8516a8
pramodgautm <pramod.767402@rrlc.tu.edu.np>
2024-06-24 13:29:17 +0545

    setting up

---
+++ javascript/variables/index.js
@@ -0,0 +1,12 @@
+console.log("hahahahah");
+/*alert("hello");
+prompt("what is your name?");
+const person = { fname: "pramod", lname: "gautam" };
+console.log(person);*/
+
+//wap that store user info about their birth year and calculate
+
+const year = Number(prompt("what is your birth year?")); //typecasting
+console.log(year);
+const age = 2024 - year;
+console.log(age);
