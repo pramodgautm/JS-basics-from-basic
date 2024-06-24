9ae5ec838419a165cf378ad757fba372b307cfe3
pramodgautm <pramod.767402@rrlc.tu.edu.np>
2024-06-24 13:41:25 +0545

    if-else statement

--- javascript/variables/index.js
+++ javascript/variables/index.js
@@ -10,3 +10,14 @@ const year = Number(prompt("what is your birth year?")); //typecasting
 console.log(year);
 const age = 2024 - year;
 console.log(age);
+
+//statement -----if else
+//age below 10 =child
+//age below 18 =teen
+//age below 29is adult
+//age above 30 =old
+if (age > 18) {
+  alert("you are adult");
+} else {
+  alert("you are child");
+}
