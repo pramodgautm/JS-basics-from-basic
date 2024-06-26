b2d1337c078b3b6381ce8856c834fd84f02bd0b2
pramodgautm <pramod.767402@rrlc.tu.edu.np>
2024-06-26 14:28:09 +0545

    multiplication using for loop

--- javascript/loop/index.js
+++ javascript/loop/index.js
@@ -1,3 +1,12 @@
-for (let i = 0; i < 100; i++) {
+/*for (let i = 0; i < 100; i++) {
   console.log(i);
 }
+console.log("hello");*/
+
+//wap for multiplication table for 3.
+
+for (let i = 0; i <= 10; i++) {
+  const result = 3 * i;
+  console.log("3*", i, "=", result);
+}
+//wap for print only even number.
